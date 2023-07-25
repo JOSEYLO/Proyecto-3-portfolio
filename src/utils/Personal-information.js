7f15b06097bbe610e82077877760682b223d5ae9
joseylo <joselee2011@hotmail.es>
2023-07-25 11:39:52 +0200

    change

--- src/utils/Personal-information.js
+++ src/utils/Personal-information.js
@@ -5,8 +5,8 @@ export const infoPersonal = [
     desc: `Haciendo una breve introducción y para que puedan 
     conocerme un poco más, comentar que a principios de 2023 tome la decision de reinventarme
      dando un giro de 360 grados en mi vida profesional.
-     Por primera vez me vi obligado a reinventarme y tropece por casualidad en el maravilloso mundo del Jamón.
-    Otra etapa que creo haber acabado`,
+     Por primera vez me vi obligado a cambiar de oficio y tropece por casualidad en el maravilloso mundo del Jamón.
+    Otra etapa que creo haber acabado.`,
   },
   {
     photo: "./assets/mecanotubo.jpg",
@@ -14,8 +14,7 @@ export const infoPersonal = [
     desc: `En cuanto a mi formación académica,
      soy tecnico superior en Planificacón urbanistica y desarrollo de proyectos topograficos.
     curse mis estudios en la universidad de Castilla La Mancha en el año 2000. Me dedique a ello durante 8 años ininterrumpidos en la ya extinta Grupo Mecanotubo,
-    hasta que irrrumpio la crisis del 2008 y desaparecio la obra civil en España por completo.
-    .`,
+    hasta que irrumpio la crisis del 2008 y desaparecio la obra civil en España por completo.`,
   },
   {
     photo: "./assets/rock-thecode.jpg",
